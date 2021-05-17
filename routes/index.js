5335583b6db80b6724cfb1b4ba635ab3f6218134
Noah <mcbeasty11.nh@icloud.com>
2021-05-16 20:42:50 -0500

    routes and http.rest files

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const apiRoutes = require('./api');
 router.use('/api', apiRoutes);
 
 router.use((req, res) => {
-  res.send('<h1>Wrong Route!</h1>');
+  res.send('<h1>Check route! Ex: http://localhost3003/api/categories</h1>');
 });
 
 module.exports = router;
